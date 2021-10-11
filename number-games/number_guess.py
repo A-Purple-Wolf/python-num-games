f0da9b4ad36be181a8524f9475ec0999617bcfed
A-Purple-Wolf <92323343+A-Purple-Wolf@users.noreply.github.com>
2021-10-11 13:24:37 -0400

    Add files via upload

--- number-games/number_guess.py
+++ number-games/number_guess.py
@@ -13,15 +13,15 @@ while guess != num and guess.isdigit():
        if guess_count >= 2: 
 
            # Start giving hints to the player.
-          if num % guess_count == 0:
+          if int(num) % guess_count == 0:
               print("The number is divisible by", guess_count) 
               # Lets them know that the number is divisible by 2, 3, etc.
 
           else:
-              for i in range(2, num):
+              for i in range(2, int(num)):
               # Checks for a prime number.
 
-                  if num % i == 0:
+                  if int(num) % i == 0:
                       print("The number is not divisible by", guess_count)
                       # The number is not divisible by guess_count but is not prime
                       break
@@ -36,9 +36,9 @@ while guess != num and guess.isdigit():
                           break   
                                    
     else: # Hints to help the player guess the prime number
-        if (num - 1) % guess_count == 0:
+        if (int(num) - 1) % guess_count == 0:
             print("The number before the prime is divisible by", guess_count)
-        elif (num + 1) % guess_count == 0:
+        elif (int(num) + 1) % guess_count == 0:
             print("The number after the prime is divisible by", guess_count)
         else:
             print("The numbers before and after the prime are not divisible by", guess_count)
